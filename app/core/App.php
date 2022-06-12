bda7f452ceec05848cc4757b0b7204979dd8e01d
saddamhf_ <muhamadsaddam17@gmail.com>
2022-06-12 20:03:54 +0800

    change htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -10,11 +10,13 @@ class App{
     public function parseURL(){
 
         if(isset($_GET['url'])){
-            $url=rtrim($_GET['url'],'/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url);
-            return $url;
+            $url= $_GET['url'];
         }
     }
 }
+
+// $url=rtrim($_GET['url'],'/');
+//             $url = filter_var($url, FILTER_SANITIZE_URL);
+//             $url = explode('/', $url);
+//             return $url;
 ?>
